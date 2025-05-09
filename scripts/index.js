fe8e9a456a2eef613bd2dcbd9abc01c0ad265f7c
Yuschinskiy <lastwitcher10@gmail.com>
2025-05-09 10:42:01 +0300

    first try

--- scripts/index.js
+++ scripts/index.js
@@ -7,3 +7,33 @@
 // @todo: Функция удаления карточки
 
 // @todo: Вывести карточки на страницу
+
+function createCard(cardData, handleDeleteCard) {
+  const cardTemplate = document.querySelector('#card-template').content.querySelector('.card');
+  const cardElement = cardTemplate.cloneNode(true);
+
+  const cardImage = cardElement.querySelector('.card__image');
+  const cardTitle = cardElement.querySelector('.card__title');
+  const deleteButton = cardElement.querySelector('.card__delete-button');
+
+  cardImage.src = cardData.link;
+  cardImage.alt = cardData.name;
+  cardTitle.textContent = cardData.name;
+
+  deleteButton.addEventListener('click', () => {
+    handleDeleteCard(cardElement);
+  });
+
+  return cardElement;
+}
+
+function deleteCard(cardElement) {
+  cardElement.remove();
+}
+
+const placesList = document.querySelector('.places__list');
+
+initialCards.forEach(cardData => {
+  const cardElement = createCard(cardData, deleteCard);
+  placesList.append(cardElement);
+});
